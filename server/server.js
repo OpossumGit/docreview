b7b3e90937232074db06cf338792fbf096ee2783
tvrtko <tvrtko.mrkonjic@gmail.com>
2018-05-24 01:21:06 +0200

    linted

--- server/server.js
+++ server/server.js
@@ -34,17 +34,16 @@ boot(app, __dirname, function(err) {
 });
 
 // Enable http session
-//app.use(loopback.session({ secret: 'keyboard cat' }));
+// app.use(loopback.session({ secret: 'keyboard cat' }));
 
 // Load the provider configurations
 var config = {};
 try {
- config = require('../providers.json');
-} catch(err) {
- console.error(err);
- console.error('Please configure your passport strategy in `providers.json`.');
- console.error('Copy `providers.json.template` to `providers.json` and replace the clientID/clientSecret values with your own.');
- process.exit(1);
+  config = require('../providers.json');
+} catch (err) {
+  console.error(err);
+  console.error('Please configure your passport strategy in `providers.json`.');
+  process.exit(1);
 }
 
 // Initialize passport
@@ -52,13 +51,13 @@ passportConfigurator.init();
 
 // Set up related models
 passportConfigurator.setupModels({
- userModel: app.models.user,
- userIdentityModel: app.models.userIdentity,
- userCredentialModel: app.models.userCredential
+  userModel: app.models.user,
+  userIdentityModel: app.models.userIdentity,
+  userCredentialModel: app.models.userCredential,
 });
 // Configure passport strategies for third party auth providers
-for(var s in config) {
- var c = config[s];
- c.session = c.session !== false;
- passportConfigurator.configureProvider(s, c);
+for (var s in config) {
+  var c = config[s];
+  c.session = c.session !== false;
+  passportConfigurator.configureProvider(s, c);
 };
